cc9a57c219be822fc27ae746295e5c50cc5f99cb
AashutoshSubedi <aashutoshsubedi18@gmail.com>
2026-05-16 09:16:19 +0545

    Add files via upload
    java programs uploaded

---
+++ src/VariablesDemo.java
@@ -0,0 +1,17 @@
+public class VariablesDemo {
+    public static void main(String[] args) {
+
+        int age = 20;
+        double height = 5.5;
+        boolean isStudent = true;
+        char grade = 'A';
+        String name = "Aashutosh";
+
+        System.out.println("Integer value: " + age);
+        System.out.println("Double value: " + height);
+        System.out.println("Boolean value: " + isStudent);
+        System.out.println("Character value: " + grade);
+        System.out.println("String value: " + name);
+
+    }
+}
